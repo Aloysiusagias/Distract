4bcb1b4a3920b77ce4fe95a251b199a084d95ade
azhaalvin <39091301+azhaalvin@users.noreply.github.com>
2020-12-11 13:24:35 +0700

    benerin
    masih

--- pages/keranjang.js
+++ pages/keranjang.js
@@ -11,193 +11,141 @@ import {
     Button,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-
-
-
+import { multiply } from 'react-native-reanimated';
 
 
 const keranjang = () => {
-    const [count, setCount] = useState(0);
-    const Cplus=() =>{
+  const navigation = useNavigation();
+  const [count, setCount, setHarga, hitung] = useState(0);
+  const Cplus=() =>{
         setCount(count + 1 <= Number.MAX_SAFE_INTEGER ? count + 1 : count)
     }
     
-    const Cminus=() =>{
-        setCount(count - 1 >= Number.MIN_SAFE_INTEGER ? count - 1 : count)
-    }
-    let {width, height} = Dimensions.get('window');
-    height = height - height * 0.035;
-    const navigation = useNavigation();
-    return(
-        <ScrollView>
-      <View style={{width: width, height: height}}>
-        <View style={styles.bgsquare} />
+  const Cminus=() =>{
+        setCount(count - 1 >= 0 ? count - 1 : count)
+  }
+  const hapusB=() =>{
+    setCount(count - 1 >= 0 ? count - count : count)
+    
+  }
+  let {width, height} = Dimensions.get('window');
+  height = height - height * 0.108;
+  return(
+  <ScrollView>
+    <View style={{width: width, height: height}}>
+      <View style={styles.bgsquare}>
         <View style={styles.bgIcon}>
-                <Image
-                source={require('../assets/logo.png')}
-                style={styles.logoAtas}
-                />
-            </View>
-        <View style={styles.container}>
-        <Text style={styles.teksMasuk}>Dancow</Text>
-          <View style={styles.containerIsi}>
-
-            <View>
-            <Text style={styles.teksHarga}>Rp.5000,-</Text>
-            </View>
-
-            <View style={{
-                flexDirection: "row",
-                alignSelf: 'center',
-                paddingTop:"10%",
-            }}>
-                <Text style={{fontSize:40,paddingRight:"10%"}}>Jumlah</Text>
+          <Image
+            source={require('../assets/bag.png')}
+            style={styles.bag}
+          />
+        </View>
+        <Text style={styles.teksToko}>TobangaDO</Text>
+      </View>   
+      <View >    
+          <View style={styles.items}>
+          <Image source={require('../assets/dancow.png')} />
+            <View style={{flexDirection:"column",marginLeft:"-25%",alignSelf:"center",}}>
+                <Text style={{fontSize: 20, color: '#0E49B5'}}>Dancow</Text>
                 <View
-                style={{
-                    flexDirection: "row",
-                    alignSelf: 'center',
-                    backgroundColor: '#E4E3E3',
-                    }}>
-                        
-                        <View style={styles.buttonStyle}>
-                            <Button
-                            onPress={Cminus}
-                            title='-' />
-                        </View>
-                        <Text style={styles.teksConter} >{count}</Text>
-                        <View style={styles.buttonStyle}>
-                            <Button
-                            onPress={Cplus}
-                            title='+' />
-                        </View>
-                        
+                      style={{
+                          flexDirection:"row",
+                          alignItems:'center',
+                          backgroundColor: '#E4E3E3',
+                          marginLeft: 10,
+                          width:"100%",
+                          alignSelf:"center",
+                          }}>
+                              
+                              <View style={styles.buttonStyle}>
+                                  <Button
+                                  onPress={Cminus}
+                                  title='-' />
+                              </View>
+                              <Text style={styles.teksConter} >{count}</Text>
+                              <View style={styles.buttonStyle}>
+                                  <Button
+                                  onPress={Cplus}
+                                  title='+' />
+                              </View>
                 </View>
             </View>
-
-
-            <TouchableOpacity
-              style={{marginBottom: '20%'}}
-              onPress={() => navigation.navigate('bottomTabs')}>
-              <View style={styles.buttonS}>
-                <Text style={styles.buttonText}>TAMBAH</Text>
-              </View>
-            </TouchableOpacity>
+            <View style={{flexDirection:"column",}}>
+                <Text>Rp.5000</Text>
+                <Button
+                onPress={hapusB}
+                title='Hapus' />
+            </View>
           </View>
-        </View>
-      </View>
-      <Image
-        style={styles.logoBawah}
-        source={require('../assets/logo_distract.png')}
-      />
-    </ScrollView>
-    );
-};
+          <View style={{flexDirection:"row",width:"100%",bottom:20,}}>
+          <Text style={{width:"80%",alignSelf:"center",paddingLeft:"10%",fontSize: 20,backgroundColor:"#F9F9F9",color:"#0E49B5"}}>Total Rp.5000</Text>
+          <View style={{width:"20%"}}>
+              <Button 
+                    onPress={hapusB}
+                    title='Beli' />
+          </View>
+          </View>
+       
+      </View> 
+    </View>
+  </ScrollView>
+
+  )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        padding: 20,
-        flex: 1,
+bgsquare: {
+  width: '100%',
+  height: '35%',
+  backgroundColor: '#0E49B5',
+  padding: '5%',
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+bgIcon: {
+  width: 100,
+  height: 100,
+  borderRadius: 130 / 2,
+  backgroundColor: '#E4E3E3',
+  padding: '5%',
+  shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 6,
       },
-      bgsquare: {
-        width: '100%',
-        height: '40%',
-        backgroundColor: '#0E49B5',
-        position: 'absolute',
-      },
-      teksMasuk: {
-        color: 'white',
-        fontSize: 40,
-        top: 110,
-        
-      },
-      teksHarga: {
-        color: 'black',
-        fontSize: 40,
-        top: '10%',
-        alignSelf: 'center',
-        
-    },
-    teksConter: {
-        color: 'black',
-        fontSize: 40,
-        justifyContent:"center",
-        alignSelf: 'center',
-        
-      },
-      containerIsi: {
-        marginTop: '40%',
-        width: '100%',
-        backgroundColor: 'white',
-        borderRadius: 15,
-        padding: '5%',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 6,
-        },
-        shadowOpacity: 0.37,
-        shadowRadius: 7.49,
-        elevation: 12,
-      },
-      input: {
-        backgroundColor: '#E4E3E3',
-        marginHorizontal: '3%',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 20,
-      },
-      inputtext: {
-        width: '80%',
-        marginLeft: '5%',
-        fontSize: 18,
-      },
-      buttonS: {
-        backgroundColor: '#0E49B5',
-        width: '80%',
-        padding: 10,
-        borderRadius: 15,
-        alignSelf: 'center',
-      },
-      buttonText: {
-        color: 'white',
-        fontSize: 20,
-        textAlign: 'center',
-      },
-      NoAkun: {
-        textAlign: 'center',
-        fontSize: 20,
-      },
-      logoBawah: {
-        position: 'absolute',
-        bottom: 0,
-        alignSelf: 'center',
-      },
-      bgIcon: {
-        width: 110,
-        height: 110,
-        borderRadius: 130 / 2,
-        backgroundColor: '#E4E3E3',
-        padding: '5%',
-        top: 20,
-        position: 'absolute',
-        alignSelf: 'center',
-      },
-      logoAtas: {
-        top: 20,
-        alignSelf: 'center',
-        width: '90%',
-        height: '65%',
-        
-      },
-      buttonStyle: {
-        width: "10%",
-        borderRadius:50,
-        margin: 10,
-      }
-    
+      shadowOpacity: 0.37,
+      shadowRadius: 7.49,
+      elevation: 12,
+},
+bag: {
+  width: '90%',
+  height: '90%',
+  
+},
+teksToko: {
+  marginTop: 10,
+  fontSize: 32,
+  color: 'white',
+ fontWeight: 'bold'
+},
+teksConter :{
+  
+  fontSize: 14,
+  width: "20%"
+},
 
-})
+produk: {
+  
+  alignItems: 'center',
+},
+items: {
+  width: Dimensions.get('window').width - 10,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: "space-between",
+  padding: '5%',
+  margin: '2%',
+},
+});
 
 export default keranjang;
